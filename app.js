e74e6ea2ebd55857053ba1af580bd8395fedafa8
bazzika <darya.belob@gmail.com>
2017-07-24 15:11:49 +0300

    add FormControl

--- app.js
+++ app.js
@@ -4,16 +4,20 @@
 var baseOfRecipes = [{ title: "Pumpkin Pie", ingredients: ["Pumpkin Puree", "Sweetened Condensed Milk", "Eggs", "Pumpkin Pie Spice", "Pie Crust"] , "id": 0},
   { title: "Spaghetti", ingredients: ["Noodles", "Tomato Sauce", "(Optional) Meatballs"],"id": 1},
   { title: "Onion Pie", ingredients: ["Onion", "Pie Crust", "Sounds Yummy right?"],id:2
-}];
+  }];
 
 var Modal = ReactBootstrap.Modal;
 var Button = ReactBootstrap.Button;
+var Accordion = ReactBootstrap.Accordion;
+var Panel=ReactBootstrap.Panel;
+var FormControl=ReactBootstrap.FormControl;
 
 
 const Recipe = React.createClass({
   getInitialState() {
     return {
-      showModal: false
+      showModal: false,
+      showRecipe: false
     };
   },
   getList: function (list) {
@@ -30,44 +34,36 @@ const Recipe = React.createClass({
       )
     }
   },
-  deleteRecipe: function (e) {
-
-    recipes.splice(e.target.id, 1);
-    localStorage.setItem('Recipes', JSON.stringify(recipes));
+  //deleteRecipe: function (e) {
+  //
+  //
+  //  this.setState({
+  //    showModal: false
+  //  });
+  //},
+  //editRecipe:function () {
+  //
+  //
+  //  });
+  //},
+  //saveEditions: function (e) {
+  //
+  //},
+  close() {
     this.setState({
+      showModal: false,
       showRecipe: false
-    });
-  },
-  editRecipe:function () {
-    this.setState({
-      childVisible: !this.state.childVisible
-    });
-  },
-  saveEditions: function (e) {
-    var recipeTitle = $("#recipeTitle").val() || 'Undefined';
-    var ingridients = $("#ingridients").val() || 'Undefined';
-    this.props.recipe.recipe = recipeTitle;
-    this.props.recipe.ingridients = ingridients;
-    recipes[e.target.id] = {
-      "recipe": recipeTitle,
-      "ingridients": ingridients,
-      "id": e.target.id
-    };
-    localStorage.setItem('Recipes', JSON.stringify(recipes));
-    this.setState({
-      childVisible: !this.state.childVisible
-    });
-  },
-    close() {
-    this.setState({
-      showModal: false
     });
   },
 
   open() {
     this.setState({
-      showModal: true
+      showModal: true,
+      showRecipe:false
     });
+  },
+  handleChange(e) {
+    this.setState({ value: e.target.value });
   },
 
   render() {
@@ -75,7 +71,19 @@ const Recipe = React.createClass({
 
     return (
       <div>
-        <p>Recipe title</p>
+        <div>
+          <Accordion>
+            <Panel header='title' id="0">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </Panel>
+            <Panel header="Collapsible Group Item #2" eventKey="2">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </Panel>
+            <Panel header="Collapsible Group Item #3" eventKey="3">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </Panel>
+          </Accordion>
+          </div>
 
         <Button
           bsStyle="primary"
@@ -87,14 +95,24 @@ const Recipe = React.createClass({
 
         <Modal show={this.state.showModal} onHide={this.close}>
           <Modal.Header closeButton>
-            <Modal.Title>Recipe title</Modal.Title>
+            <Modal.Title>Recipe</Modal.Title>
+            <FormControl
+              type="text"
+              value={this.state.value}
+              placeholder="Enter text"
+              onChange={this.handleChange}
+              />
+            <FormControl.Feedback />
           </Modal.Header>
           <Modal.Body>
             <h4>Ingridients</h4>
-            <p> bla-bla</p>
-            //<hr />
-            //
-            //<h4>Overflowing text to show scroll behavior</h4>
+            <FormControl
+              type="text"
+              value={this.state.value}
+              placeholder="Enter ingrients throuh coma"
+              onChange={this.handleChange}
+              />
+            <FormControl.Feedback />
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.close}>Close</Button>
@@ -117,7 +135,7 @@ const Recipe = React.createClass({
 //});
 ReactDOM.render(
   //<Button bsStyle="primary">Primary</Button>,
- <Recipe />,
+  <Recipe />,
 
   document.querySelector('.box')
 );
